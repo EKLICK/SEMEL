c2e7854849d6a829dbada9a3343a32862c31c633
EKLICK <eklickupgrade@hotmail.com>
2018-11-12 09:18:49 -0200

    Instalado a packpage Decomposer no projeto

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('decompose','\Lubusin\Decomposer\Controllers\DecomposerController@index');
 
 Route::get('/', function () {
     return view('welcome');
