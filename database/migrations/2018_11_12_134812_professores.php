d4b4fbfbf39c093832f21d2fc215db659d8b7a6c
EKLICK <eklickupgrade@hotmail.com>
2018-11-12 11:51:26 -0200

    Adicionaro ao banco de dados a tabela professores

---
+++ database/migrations/2018_11_12_134812_professores.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Professores extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome');
+            $table->string('matricula')->nullable();
+            $table->string('telefone')->nullable();
+            $table->string('email')->nullable();
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
