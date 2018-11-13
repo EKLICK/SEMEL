215ba79174b1b105a6c233bf223e93719e98258e
EKLICK <eklickupgrade@hotmail.com>
2018-11-13 08:31:25 -0200

    Design da pagina de registros de professores criada

--- app/Http/Controllers/professorController.php
+++ app/Http/Controllers/professorController.php
@@ -13,7 +13,7 @@ class professorController extends Controller
      */
     public function index()
     {
-        //
+        return view ('professores');
     }
 
     /**
--- resources/views/professores.blade.php
+++ resources/views/professores.blade.php
@@ -1,7 +1,25 @@
-@extends('layout.app')
+@extends('layouts.app')
 
 @section('content')
-    <div style="margin: 5%, 5%, 5%, 5%; background-color: white;">
+    <div class="white" style="margin-top: 3%; margin-left: 3%; margin-right: 3%;">
+        <table class="centered">
+            <thead>
+                <tr>
+                    <th>Nome do professor</th>
+                    <th>Matricula</th>
+                    <th>Melefone</th>
+                    <th>E-mail</th>
+                    <th>Editar</th>
+                    <th>Deletar</th>
+                </tr>
+            </thead>
 
+            <tbody>
+                <tr>
+                    
+                </tr>
+            </tbody>
+        </table>
+        <a href=""><i class="large material-icons" style="color: green;">add_circle_outline</i></a>
     </div>
 @endsection
