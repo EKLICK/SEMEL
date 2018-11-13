9af50f7ef955c9736c050d2377c4a9ea89e9de60
EKLICK <eklickupgrade@hotmail.com>
2018-11-13 11:55:04 -0200

    Criado a classe Pessoa

---
+++ app/Pessoa.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pessoa extends Model
+{
+    protected $fillable = [
+        'nome, nascimento', 'sexo', 'rg', 'cpf', 'cidade', 'endereco', 'bairro',
+        'cep', 'telefone', 'estado_civil', 'nome_do_pai', 'nome_da_mae', 'pessoa_emerncia',
+        'telefone_emergencia', 'filhos', 'convenio_medico', 'irmaos', 'mora_com_os_pais', 'inativo',
+    ];
+
+    public function anamneses(){
+        return $this->belongsTo(Anamnese::class);
+    }
+
+    public function turmas(){
+        return $this->belongsToMany(Anamnese::class, 'turmas_pessoas');
+    }
+}
---
+++ resources/views/pessoas_file/pessoas.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="white container" style="margin-top: 3%;">
+        <table class="centered">
+            <thead>
+                <tr>
+                    <th>Nome do professor</th>
+                    <th>Matricula</th>
+                    <th>Telefone</th>
+                    <th>E-mail</th>
+                    <th>Editar</th>
+                    <th>Deletar</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($professoreslist as $professor)
+                    <tr>
+                        <td><h5>{{$professor->nome}}</h4></td>
+                        <td><h5>{{$professor->telefone}}</h5></td>
+                        <td><h5>{{$professor->telefone_emergencia}}</h5></td>
+                        <td><h5>{{$professor->telefone_bairro}}</h5></td>
+                        <td><a href="{{Route('professor.edit', $professor->id)}}"><i class="material-icons medium" style="color: green;">edit</i></a></td>
+                        <td><a id="btn-delete" data-id="{{$professor->id}}" data-nome="{{$professor->nome}}" href="#modaldelete" class="modal-trigger"><i class="material-icons medium" style="color: green;">delete</i></a></td>
+                    </tr>
+                @endforeach 
+            </tbody>
+        </table>
+        <a href="{{route('professor.create')}}"><i class="medium material-icons" style="color: green;">add_circle_outline</i></a>
+    </div>
+
+    <div id="modaldelete" class="modal">
+        <form action="{{route('professor.destroy', 'delete')}}" method="POST">
+            @method('DELETE')
+            @csrf
+            <div class="modal-content">
+                <h4>Deletar</h4>
+                <p>Você tem certeza que deseja deletar o professor abaixo?</p>
+                <div class="row">
+                    <label for="name_delete">Nome:</label>
+                    <div class="input-field col s12">
+                        <input class="validate" hidden name="id" type="number" id="id_delete">
+                        <input disabled class="validate" type="text" id="name_delete">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn red delete" type="submit">Sim</button>
+            </div>
+        </form>
+    </div>
+@endsection
