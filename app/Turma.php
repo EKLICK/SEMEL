8b54304967b94628797b97a6afff42ab42e94ff1
EKLICK <eklickupgrade@hotmail.com>
2018-11-13 12:20:21 -0200

    Criado a classe Turma

---
+++ app/Turma.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Turma extends Model
+{
+    protected $fillable = [
+        'nome', 'limite', 'nucleo_id',
+    ];
+
+    public function pessoas(){
+        return $this->hasMany(Pessoa::class);
+    }
+
+    public function nucleos(){
+        return $this->BelongsTo(Nucleo::class);
+    }
+}
