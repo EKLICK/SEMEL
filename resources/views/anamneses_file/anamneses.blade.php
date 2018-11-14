0b0ee06475aa6b20e0b545907a11fb96a90411f5
EKLICK <eklickupgrade@hotmail.com>
2018-11-14 13:58:49 -0200

    Ajustes na pagina de Anamneses feitos

--- resources/views/anamneses_file/anamneses.blade.php
+++ resources/views/anamneses_file/anamneses.blade.php
@@ -15,10 +15,10 @@
             <tbody>
                 @foreach ($anamneseslist as $anamnese)
                     <tr>
-                        <td><h5>{{$anamnese->pessoas->nome}}</h4></td>
+                        <td><h5>@if(isset($anamnese->pessoas->nome)) {{$anamnese->pessoas->nome}} @else Usuário não cadastrado @endif</h4></td>
                         <td><h5>{{$anamnese->peso}}</h5></td>
                         <td><h5>{{$anamnese->altura}}</h5></td>
-                        <td><h5>{{$anamnese->dor_articular}}</h5></td>
+                        <td><h5>@if(isset($anamnese->doencas->nome)) {{$anamnese->doencas->nome}} @else Sem doenças @endif</h5></td>
                         <td><a href="{{Route('anamneses.edit', $anamnese->id)}}"><i class="material-icons medium" style="color: green;">edit</i></a></td>
                     </tr>
                 @endforeach 
