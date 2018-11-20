a3b107d476ee01f9a80e9b7fc5dbfe8a8dd90ecf
EKLICK <eklickupgrade@hotmail.com>
2018-11-20 14:03:11 -0200

    Mudanças na pagina de turmas_nucleos

--- resources/views/nucleos_file/nucleos_turmas.blade.php
+++ resources/views/nucleos_file/nucleos_turmas.blade.php
@@ -7,8 +7,6 @@
                 <tr>
                     <th>Nome da turma</th>
                     <th>limite</th>
-                    <th>Editar</th>
-                    <th>Deletar</th>
                 </tr>
             </thead>
             <tbody>
@@ -16,33 +14,9 @@
                     <tr @if(count($turma->pessoas) >= $turma->limite) class="yellow darken-2" @endif>
                         <td><h5>{{$turma->nome}}</h4></td>
                         <td><h5>{{count($turma->pessoas)}} / {{$turma->limite}}</h5></td>
-                        <td><a href="{{Route('turmas.edit', $turma->id)}}"><i class="material-icons medium" style="color: green;">edit</i></a></td>
-                        <td><a id="btn-delete" data-id="{{$turma->id}}" data-nome="{{$turma->nome}}" href="#modaldelete" class="modal-trigger"><i class="material-icons medium" style="color: green;">delete</i></a></td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
-        <a href="{{route('turmas.create')}}"><i class="medium material-icons" style="color: green;">add_circle_outline</i></a>
-    </div>
-
-    <div id="modaldelete" class="modal">
-        <form action="{{route('turmas.destroy', 'delete')}}" method="POST">
-            @method('DELETE')
-            @csrf
-            <div class="modal-content">
-                <h4>Deletar</h4>
-                <p>Você tem certeza que deseja deletar o professor abaixo?</p>
-                <div class="row">
-                    <label for="name_delete">Nome:</label>
-                    <div class="input-field col s12">
-                        <input class="validate" hidden name="id" type="number" id="id_delete">
-                        <input disabled class="validate" type="text" id="name_delete">
-                    </div>
-                </div>
-            </div>
-            <div class="modal-footer">
-                <button class="btn red delete" type="submit">Sim</button>
-            </div>
-        </form>
     </div>
 @endsection
